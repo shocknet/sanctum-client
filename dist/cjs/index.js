92ce468416cff1cda0c12eff83b690ac53f02ee4
Mothana <snakeeaterrdev@gmail.com>
2024-12-11 21:02:00 +0400

    point to prod sanctum

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -107,8 +107,8 @@ class ClientKeyManager {
 }
 
 const config = {
-    SANCTUM_URL: "https://auth.shocklab.dev",
-    SANCTUM_WS_URL: "wss://auth.shocklab.dev",
+    SANCTUM_URL: "https://auth.shock.network/",
+    SANCTUM_WS_URL: "https://auth.shock.network/",
 };
 // Validate config at build time
 Object.entries(config).forEach(([key, value]) => {
--- dist/esm/utils/config.js
+++ dist/esm/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    SANCTUM_URL: "https://auth.shocklab.dev",
-    SANCTUM_WS_URL: "wss://auth.shocklab.dev",
+    SANCTUM_URL: "https://auth.shock.network/",
+    SANCTUM_WS_URL: "https://auth.shock.network/",
 };
 // Validate config at build time
 Object.entries(config).forEach(([key, value]) => {
